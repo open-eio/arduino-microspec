d40edb38dbf137c46654c52e7f8ce099ff1316af
Craig Wm. Versek <cversek@gmail.com>
2017-03-08 02:12:45 -0500

    live matplolib viz

---
+++ legacy/read_spec.py
@@ -0,0 +1,35 @@
+import glob, json
+import serial
+
+port = glob.glob("/dev/ttyACM*")[-1]
+
+ser = serial.Serial(port,baudrate=115200)
+
+from pylab import *
+from matplotlib import pyplot as plt
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+plt.ion()
+
+fig.canvas.draw()
+
+l1, = ax1.plot(arange(288),[0]*288)
+
+ax1.set_xlim(0,288)
+ax1.set_ylim(0,1024)
+plt.show()
+
+
+while True:
+    try:
+        line = ser.readline()
+        data = json.loads(line.decode('utf8'))
+        spec = array(data)
+        print(spec)
+        l1.set_xdata(arange(288))
+        l1.set_ydata(spec)
+        #ax1.draw_artist(l1)
+        fig.canvas.draw()
+        fig.canvas.flush_events()
+    except ValueError:
+        pass
